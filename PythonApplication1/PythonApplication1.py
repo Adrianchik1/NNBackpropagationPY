Fixed delta calculation

PythonApplication1/PythonApplication1.py
@@ -1,22 +1,21 @@
 import numpy as np
-import nnfs
-from nnfs.datasets import spiral_data
 
 from activations import Activation_ReLU, Activatioin_Softmax
 from weightsBiases import Layer_Dense
 from iterate import iteration               #importing needed functions from another files
 from logicManager import Optimiser
 from charts import makeChart
+from losses import MSELoss
+from generateData import generateData
 
-nnfs.init()
 loss = float('inf')         #starting loss
-iterations = 1000            #amount of iterations, how much times it would run the optimisation
+iterations = 100000            #amount of iterations, how much times it would run the optimisation
 change = 0.05               #amount by which weights and biases should change each iteration
 losses = []                 #array to store the last 10 losses
 differenceOfLosses = []     #array to store the differences of the last two losses(for future analysis)
 implementBiases = False
 
-dense1 = Layer_Dense(2, 10)         #weights and biases in the first layer
+dense1 = Layer_Dense(10, 10)         #weights and biases in the first layer
 activation1 = Activation_ReLU()     #an activation class, that would multiply weights by inputs and would add biases
 
 dense2 = Layer_Dense(10, 5)         #weights and biases in the second layer
@@ -25,22 +24,23 @@ activation2 = Activation_ReLU()     #another one activation class
 dense3 = Layer_Dense(5, 3)          #weights and biases in the output layer
 activation3 = Activatioin_Softmax() # 
 
-X, y = spiral_data(samples=100, classes=3)
-
 denses = [dense1, dense2, dense3]                       #all denses are added to one array, to pass them to another functions
 activations = [activation1, activation2, activation3]   #all activations are added to one array, to pass them to another functions
+
+X, y = generateData()
 
 optimiser = Optimiser(X, y, activations, change)        #creating an optimiser object, in future it would do the optimisation
 
 for i in range(0, iterations):          #cycle which will optimize NN the required number of times
     print(f"Iteration {i}")
-    loss, denses = optimiser.optimise(loss, denses)     #optimising denses
+    loss = iteration(X, y, denses, activations)
+    optimiser.optimise()     #optimising denses
     print(f"Loss {loss}")
 
     losses.append(loss)                                                                     # appending the loss to the losses array
     lastTenLosses = np.array(losses[-10:])                                                  # getting the last 10 losses
-    if np.allclose(lastTenLosses, lastTenLosses[0]) == True and len(losses) > 11:           # if loss hasnt changed in the last 10 iterations breaks
-        break
+    #if np.allclose(lastTenLosses, lastTenLosses[0]) == True and len(losses) > 11:           # if loss hasnt changed in the last 10 iterations breaks
+    #    break
     
     if len(losses) > 2 : differenceOfLosses.append(losses[i-1] - losses[i])                 # appends the change in loss to the progressOfLosses array
 

PythonApplication1/logicManager.py
@@ -2,6 +2,9 @@ import numpy as np
 import copy
 from iterate import iteration
 from weightsBiases import Layer_Dense
+from calculateZ import CalculateZ
+from calculateA import CalculateA
+from losses import MSELoss
 
 class Optimiser():
     def __init__(self, X, y, activations, change):
@@ -10,58 +13,31 @@ class Optimiser():
         self.activations = activations
         self.change = change
 
-    def optimiseSingleWeight(self, loss, denses, nthElem):
-        
-        positiveDenses = Layer_Dense.ChangeWeight(denses, nthElem, self.change)     #change Nth weight by change
-        negativeDenses = Layer_Dense.ChangeWeight(denses, nthElem, -self.change)
+    def compute_delta_l(self, W_next, delta_next, z_l, activation_derivative):
+        propagated = W_next @ delta_next  # shape (n_l, 1)
+        sigma_prime = activation_derivative(z_l)  # shape (n_l, 1)
+        delta_l = propagated * sigma_prime  # element-wise multiplication
+        return delta_l
 
-        positiveLoss = iteration(self.X, self.y, positiveDenses, self.activations)  #calculates new loss
-        negativeLoss = iteration(self.X, self.y, negativeDenses, self.activations)
-        loss = iteration(self.X, self.y, denses, self.activations)                  #calculates loss with old weights
+    def gradient(self, delta_l, a_prev):
+        return np.dot( a_prev, delta_l.T)/ len(self.X)
 
-        losses = [(positiveLoss, positiveDenses),
-                  (negativeLoss, negativeDenses),
-                  (loss, denses)] 
-        return min(losses, key=lambda x: x[0])      #comperse losses, and outputs the best loss and denses
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
 
-    def optimiseSingleBias(self, loss, denses, nthElem):
-        positiveDenses = Layer_Dense.ChangeBias(denses, nthElem, self.change)
-        negativeDenses = Layer_Dense.ChangeBias(denses, nthElem, -self.change)
+    def sigmoid_derivative(self, z):
+        s = self.sigmoid(z)
+        return s * (1 - s)
 
-        positiveLoss = iteration(self.X, self.y, positiveDenses, self.activations)
-        negativeLoss = iteration(self.X, self.y, negativeDenses, self.activations)
+    def optimise(self):
+        numberOfLayers = len(Layer_Dense.LayerDenseInstances)
+        deltas =  [None for _ in range(numberOfLayers)]
+        deltas[-1] = (MSELoss.backward())  
+        for i in range((numberOfLayers-1), 0, -1):
+            delta = self.compute_delta_l(Layer_Dense.LayerDenseInstances[i].weights, deltas[i], CalculateZ.CalculateZInstances[0].z[i-1], self.sigmoid_derivative)
+            deltas[i-1] = delta
 
-        losses = [(positiveLoss, positiveDenses),
-                  (negativeLoss, negativeDenses),
-                  (loss, denses)] 
-        return min(losses, key=lambda x: x[0]) 
-     
-     
-    def calculateAmountOfWeights(self, denses):
-        amountOfWeights = []
-        for dense in denses:
-            amountOfWeightsInDense = len(dense.weights.flatten())
-            amountOfWeights.append(amountOfWeightsInDense)
-        return amountOfWeights
-
-    def whichBias(self, denses, amountOfWeights, whichWeight):
-        whichBias = 0
-        for i in range(len(denses)):
-            if amountOfWeights[i] > whichWeight:
-                whichBias += whichWeight % len(denses[i].biases[0])
-                return whichBias
-            else:
-                whichWeight -= amountOfWeights[i]
-                whichBias += len(denses[i].biases[0]) 
-
-
-    def optimise(self, loss, denses):
-        amountOfWeights = self.calculateAmountOfWeights(denses)             #calculates amount of weights in every dense(posible output: [35, 15, 9])
-        for whichWeight in range(Layer_Dense.totalAmountOfWeights):         #changes one weight per iteration
-            whichBias = self.whichBias(denses, amountOfWeights, whichWeight)        #calculates which bias should be changed
-            loss, denses = self.optimiseSingleWeight(loss, denses, whichWeight)     #optimises single weight
-            #whichBias = whichWeight//len(denses[0].biases[0])
-            loss, denses = self.optimiseSingleBias(loss, denses, whichBias)
-
-        return loss, denses
+        for i, dense in enumerate(Layer_Dense.LayerDenseInstances):
+            dense.weights -= self.change * self.gradient(deltas[i], CalculateA.a[i])
+            dense.biases -= self.change * deltas[i].T.mean(axis=0, keepdims=True)
 

PythonApplication1/losses.py
@@ -7,14 +7,33 @@ class Loss:
         return data_loss
 
 class Loss_CategoricalCrossentrophy(Loss): 
-    def forward(self, y_pred, y_true):
-        samples = len(y_pred)
-        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+    def forward(self, predictions, targets):
+        samples = len(predictions)
+        predictions_clipped = np.clip(predictions, 1e-7, 1-1e-7)
 
-        if len(y_true.shape) == 1:
-            correct_confidences = y_pred_clipped[range(samples), y_true]
-        elif len(y_true.shape) == 2:
-            correct_confidences = np.sum(y_pred_clipped*y_true, axis = 1)
+        if len(targets.shape) == 1:
+            correct_confidences = predictions_clipped[range(samples), targets]
+        elif len(targets.shape) == 2:
+            correct_confidences = np.sum(predictions_clipped*targets, axis = 1)
 
         negative_log_likelihoods = -np.log(correct_confidences)
         return negative_log_likelihoods
+
+class MSELoss(Loss):
+    diff = None
+    # MSELossInstances = []
+    # def __init__(self):
+    #     self.MSELossInstances.append(self)
+    def forward(self, predictions, targets):
+        predictions = np.array(predictions)
+        targets = np.array(targets)
+
+        MSELoss.diff = predictions - targets
+        mse = np.mean(self.diff ** 2)
+        return mse
+    @staticmethod
+    def backward():
+        # diff_single = MSELoss.diff[0]  # shape (output_size,)
+        # delta = (2 * diff_single / len(MSELoss.diff)).reshape(-1, 1)  # shape (output_size, 1)
+        #return delta
+        return (2 * MSELoss.diff / len(MSELoss.diff)).T