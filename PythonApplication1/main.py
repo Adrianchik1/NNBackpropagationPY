8e27c3eae93c614d517fe78a731c5d7194bb7540
Adrianchik1 <adrian2.lutsyk2010@gmail.com>
2026-01-13 13:36:19 +0100

    added photos to read me

--- PythonApplication1/main.py
+++ PythonApplication1/main.py
@@ -9,7 +9,7 @@ from losses import MSELoss
 from generateData import generateData
 
 loss = float('inf')         #starting loss
-iterations = 100000            #amount of iterations, how much times it would run the optimisation
+iterations = 1000            #amount of iterations, how much times it would run the optimisation
 change = 0.05               #amount by which weights and biases should change each iteration
 losses = []                 #array that stores the last 10 losses
 differenceOfLosses = []     #array to store the differences of the last two losses(for future analysis)
@@ -37,7 +37,7 @@ for i in range(0, iterations):          #cycle which will optimize NN the requir
     print(f"Loss {loss}")
 
     losses.append(loss)                                                                     # appending the loss to the losses array
-    if len(losses) > 2 : differenceOfLosses.append(losses[i-1] - losses[i])                 # appends the change in loss to the progressOfLosses array
+    if len(losses) > 1 : differenceOfLosses.append(losses[i-1] - losses[i])                 # appends the change in loss to the progressOfLosses array
 
 
 print(loss)
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-An example of a structure of a neural network using Backpropagation
+An example of a structure of a neural network using Backpropagation.
+
+Python version: 3.9.6 NumPy version: 2.0.2 Matplotlib version: 3.9.4
+
+You can adjust both the number of iterations and the magnitude by which the weights and biases are updated in each iteration. At the end of the program, it will display two graphs: the first shows the overall change in loss per iteration, while the second illustrates how the loss varies with each iteration.
+
+The graphs could look like this:
+![changeOfLoss](images/changeOfLoss.png)
+![examchangeOfLossPerIterationple](images/changeOfLossPerIteration.png)
